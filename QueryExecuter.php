52d295baa3dca93d10fc085ffaa034527b724eba
Simone Botti <botti.simone07@gmail.com>
2017-03-18 11:59:03 +0100

    Create QueryExecuter.php

---
+++ QueryExecuter.php
@@ -0,0 +1,39 @@
+<?php
+	//Call it the first time to get the PDO object
+	function connect(){
+    	try{
+   	   		$pdo = new PDO('mysql:host=localhost;dbname=my_grvepica', 'grvepica', '');
+   	   		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   	   		$pdo->exec('SET NAMES "utf8"');
+ 		}catch(PDOException $e){
+ 	  		$output = 'Server connection failed' . $e->getMessage();
+	  		exit();
+		}
+        return $pdo;
+ 	}
+    //Function that executes query
+    	//First:PDO
+    	//Second:Query
+        //Third-Infinito:Parameters
+	function executeQuery(){
+    	$pdo=func_get_arg(0);
+        $sql=func_get_arg(1);
+      try{
+      		$query=$sql;
+            $s = $pdo->prepare($query);
+            for($i=2;$i<func_num_args();$i++){
+        	 $s->bindValue(($i-1), func_get_arg($i));
+        	}
+            $s->execute();
+
+     }catch(PDOException $e){
+          $error = 'ERROR in research player' . $e->getMessage();
+          //include 'error.html.php';
+          echo "Error in Query". $e->getMessage();
+          exit();
+     }
+     return $s;
+}
+
+
+?>
