70a00f5f42c948cd6daf48a6ec4c95a9a58d3d0c
Simone Botti <botti.simone07@gmail.com>
2017-09-20 18:40:02 +0200

    Add files via upload

--- Controller.php
+++ Controller.php
@@ -4,18 +4,18 @@
 		public $dsn;
 		public $username;
 		public $password;		
-		private $queryExe;
+		private $queryExe;		private $rows=0;
 		
 		function __construct($ds,$user,$pass){			$this->dsn=$ds;			$this->username=$$user;			$this->password=$pass;
 			$this->queryExe=new QueryExecuter($this->dsn,$this->username,$this->password);
 		}
 		
-		function getData(){
-			$this->queryExe->connect();
-			$sth=$this->queryExe->Execute(func_get_args());
-			$result = $sth->fetchAll();
-			return $result;
-		}
+		function getData(){
+			$this->queryExe->connect();
+			$sth=$this->queryExe->Execute(func_get_args());
+			$result = $sth->fetchAll();			$this->rows=count($result);
+			return $result;
+		}		function getRowCount(){			return $this->rows;		}		function hasResult(){			if($this->rows==0){				return false;			}else{				return true;			}		}
 		
 	}
 
