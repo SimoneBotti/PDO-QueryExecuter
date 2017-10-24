d6c8e8e8adf6a3641e9c5f203825bd574fa0d2c3
Simone Botti <botti.simone07@gmail.com>
2017-10-24 16:06:15 +0200

    Add files via upload

--- Controller.php
+++ Controller.php
@@ -4,18 +4,17 @@
 		public $dsn;
 		public $username;
 		public $password;		
-		private $queryExe;		private $rows=0;
+		private $queryExe;		private $rows=0;				private $Ssql="SELECT Id FROM ";		private $Esql="WHERE Email=? AND Password=?";
 		
 		function __construct($ds,$user,$pass){			$this->dsn=$ds;			$this->username=$$user;			$this->password=$pass;
 			$this->queryExe=new QueryExecuter($this->dsn,$this->username,$this->password);
-		}
-		
+		}				function execute(){			$this->queryExe->connect();			$sth=$this->queryExe->execute(func_get_args());			if($sth==0){				return false;			}else{				return true;			}		}
 		function getData(){
 			$this->queryExe->connect();
-			$sth=$this->queryExe->Execute(func_get_args());
+			$sth=$this->queryExe->executeSelect(func_get_args());
 			$result = $sth->fetchAll();			$this->rows=count($result);
 			return $result;
-		}		function getRowCount(){			return $this->rows;		}		function hasResult(){			if($this->rows==0){				return false;			}else{				return true;			}		}
+		}		function getRowCount(){			return $this->rows;		}		function hasResult(){			if($this->rows==0){				return false;			}else{				return true;			}		}						//Function login		//Execute the query		//Return if the user is logged or not		function login($tableName,$email,$password){			$sql=$this->Ssql.$tableName.$this->Esql;			$result=$this->getData($sql,$email,$password);			if($this->getRowCount()==1){				return 1;			}else{				return 0;			}		}
 		
 	}
 
--- QueryExecuter.php
+++ QueryExecuter.php
@@ -26,10 +26,33 @@
 			}
 			return $this->pdo;
 		}
+		
+
+		//Function that executes query
+    	//First:Query
+        //Second-Infinity:Parameters
+		function execute(){
+			$sql=func_get_arg(0);
+			try{
+				$query=$sql[0];
+				$s = $this->pdo->prepare($query);
+				for($i=1;$i<count($sql);$i++){
+					echo $sql[$i];
+					$s->bindValue(($i), $sql[$i]);
+				}
+				$s->execute();
+			}catch(PDOException $e){
+				$error = 'ERROR in research player' . $e->getMessage();
+				//include 'error.html.php';
+				echo "Error in Query". $e->getMessage();
+				exit();
+			}
+			return $s;
+}
 		//Function that executes query
     	//First:Query
         //Second-Infinito:Parameters
-		function execute(){
+		function executeSelect(){
 			$sql=func_get_arg(0);
 			try{
 				$query=$sql;
