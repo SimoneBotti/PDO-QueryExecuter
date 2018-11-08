85121811ab3354c2eb76b3d1a5d7ecc0c1ba0944
Stiv <stivb@outlook.it>
2018-11-08 12:40:19 +0100

    fix inizializeLib

--- Lib/InizializeLib.php
+++ Lib/InizializeLib.php
@@ -1,10 +1,10 @@
 <?php
-	require_once 'Controller.php';
-    
-    $dsn = 'mysql:host=localhost;dbname=YOURDBNAME';
-	$user = "YOUR USERNAME";
+    require_once 'Controller.class.php';
+
+    $dsn = "mysql:host=localhost;dbname=YOURDBNAME";
+    $user = "YOUR USERNAME";
     $pass = "YOUR PASSWORD";
-    
+
     $controller = new Controller($dsn, $user, $pass);
 
 
